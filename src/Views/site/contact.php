930d9c3261e2fd8ae6a4377ce341acd43cddad85
B2014602 <142853228+taolaquan000@users.noreply.github.com>
2023-11-19 20:27:50 +0700

    Giao dien lien lac

---
+++ src/Views/site/contact.php
@@ -0,0 +1,65 @@
+<!-- contact -->
+<section class="contact">
+   <div class="container">
+      <div class="row mt-4 mb-50 pc">
+
+         <div class="col-12 contact__map">
+            <iframe
+               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.84151844204!2d105.76804037398634!3d10.029933690076973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a0895a51d60719%3A0x9d76b0035f6d53d0!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBD4bqnbiBUaMah!5e0!3m2!1svi!2s!4v1700390749720!5m2!1svi!2s"
+               width="1100" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
+               tabindex="0"></iframe>
+         </div>
+      </div>
+
+      <div class="row mt-4 mb-4">
+         <div class="col-lg-6 col-md-6 col-sm-12 contact__self">
+            <h3 class="mb-4">
+               Liên hệ với chúng tôi
+            </h3>
+            <p>
+               Để không ngừng nâng cao chất lượng dịch vụ và đáp ứng tốt hơn nữa các yêu cầu sử dụng sách của Quý khách,
+               chúng tôi mong muốn nhận được các thông tin phản hồi. Nếu Quý khách có bất kỳ thắc mắc hoặc đóng góp nào,
+               xin vui lòng liên hệ với chúng tôi theo thông tin dưới đây. Chúng tôi sẽ phản hồi lại Quý khách trong
+               thời gian sớm nhất.
+            </p>
+            <h3 class="mt-4 mb-4">
+               Thông tin liên hệ
+            </h3>
+
+            <ul class="contact__self-list">
+               <li class="contact__self-item">
+                  <a class="contact__self-link" href="#">qnd.com</a>
+               </li>
+               <li class="contact__self-item">
+                  <a class="contact__self-link" href="#">SĐT: 033.7691.013</a>
+               </li>
+               <li class="contact__self-item">
+                  <a class="contact__self-link" href="#">Email: qnd2023@gmail.com</a>
+               </li>
+               <li class="contact__self-item">
+                  <a class="contact__self-link" href="#">Địa chỉ: Khu 2, Đ.3/2, P.Xuân Khánh, Q.Ninh Kiều, TP.Cần Thơ</a>
+               </li>
+            </ul>
+         </div>
+         <div class="col-lg-6 col-md-6 col-sm-12 contact__regist">
+            <h3 class="mb-4">
+               Đăng ký tư vấn miễn phí
+            </h3>
+
+            <p>Quý khách vui lòng để lại thông tin để nhân viên tư vấn điện lại cho bạn sớm nhất!</p>
+
+            <form>
+               <input type="text" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Họ tên của bạn...">
+
+               <input type="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email của bạn...">
+
+               <input type="number" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Số điện thoại...">
+
+               <textarea name="" id="" cols="200" rows="15" placeholder="Nội dung cần tư vấn..."></textarea>
+               <button type="submit">Gửi liên hệ</button>
+            </form>
+         </div>
+      </div>
+   </div>
+</section>
+<!--end contact -->
